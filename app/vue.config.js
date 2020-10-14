c26678bc07009c0671b0609675000aedbe407e1b
bws9000 <armorsoft@gmail.com>
2020-10-14 13:00:37 -0400

    vue.config

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,8 +3,7 @@ module.exports = {
         proxy: {
             '^/api': {
                 target: 'http://localhost:3000',
-                ws: true,
-                changeOrigin: true
+                secure:false
             },
         }
     }
