059b927fb43c9631039f6ebc322d4f7ebd4bb3a6
bradfall <bradfall@DESKTOP-6245RT2.localdomain>
2020-10-29 10:57:34 -0400

    Committer: bradfall <bradfall@DESKTOP-6245RT2.localdomain>  On branch main  Your branch is up to date with 'origin/main'. 	modified:   src/view/dash/buy.vue 	modified:   src/view/dash/sell.vue

--- app/src/view/dash/buy.vue
+++ app/src/view/dash/buy.vue
@@ -1,7 +1,15 @@
 <template>
   <DashLayout>
-    <div class="content">Buyer, click those seller mutations you would like to purchase</div>
-  </DashLayout>
+    <div class="content"><b>If you would like to purchase a DNA mutation select below and submit.</b></div>
+<select multiple>
+  <option>Mut1</option>
+  <option>Mut2</option>
+  <option>Mut3</option><br>
+</select> 
+ <li>
+      <button type="submit">Submit purchase</button>
+        </li>
+ </DashLayout>
 </template>
 <script>
 import DashLayout from "@/layouts/dashLayout";
--- app/src/view/dash/sell.vue
+++ app/src/view/dash/sell.vue
@@ -1,8 +1,26 @@
 <template>
   <DashLayout>
       <div class="content">
-Seller you have three mutations that are associated with Hyper Cariomyopathy If you would like to sell your sequence data to researchers please click below
-//<CreateUserForm/>
+Your DNA sequence indicates that you have three mutations that are associated with Hyper Caridomyopathy. If you would like to sell your mutation sequence data to researchers please check the appropriate box below and submit.
+   <form>
+ <ul>
+    <li>
+  <input type="checkbox" id="gene1" name="gene1" value="betaMHC">
+  <label for="gene1"> betaMHC </label>
+    </li>
+   <li>
+<input type="checkbox" id="gene2" name="gene2" value="cTNT">
+  <label for="gene3"> cTNT </label>
+   </li>
+<li>
+  <input type="checkbox" id="gene3" name="gene3" value="MyBPC">
+  <label for="vehicle3"> MyBPC </label>
+ </li>
+     <li>
+ <button type="submit">Sell Mutation</button>
+        </li>
+</ul>
+</form>
 </div>
   </DashLayout>
 </template>
@@ -15,7 +33,7 @@ export default {
   components: {
     DashLayout,
    // CreateSellerForm
- //  CreateUserForm
+//   CreateUserForm
   },
   data() {
     return {
